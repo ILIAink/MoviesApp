4edceea30c28a06adde3af5d7c2bf00f7b782a72
JCDster <wzg6qh@virginia.edu>
2025-04-08 10:24:21 -0400

    Added Create Show

--- backend/db/queries.js
+++ backend/db/queries.js
@@ -32,15 +32,55 @@ const createMovie = async (
   }
 };
 
+//Create a show
+const createShow = async (
+    Show_name,
+    Season_count,
+    Genre,
+    Age_rating
+) => {
+  const client = await pool.connect();
+  try {
+    await client.query("BEGIN");
+    // RETURNING * just gives us back the newly created movie to see if everything worked as expected
+    const result = await client.query(
+        "INSERT INTO show (Show_name, Season_count, Genre, Age_rating) VALUES ($1, $2, $3, $4) RETURNING *",
+        [Show_name, Season_count, Genre, Age_rating]
+    );
+    await client.query("COMMIT");
+    return result.rows[0];
+  } catch (error) {
+    await client.query("ROLLBACK");
+    throw error;
+  } finally {
+    client.release();
+  }
+};
+
+
+//Get a user's liked movies
 const getLikedMovies = async (
     user_id
 ) => {
   const result = await pool.query(
-      "SELECT * FROM movie WHERE Movie_id IN (SELECT Movie_id FROM Likes_Movie WHERE Users_id = $1)",
+      "SELECT * FROM Movie WHERE Movie_id IN (SELECT Movie_id FROM Likes_Movie WHERE Users_id = $1)",
       [user_id]
   );
   return result.rows;
 };
 
+
+//get a users liked shows
+const getLikedShow = async (
+    user_id
+) => {
+  const result = await pool.query(
+      "SELECT * FROM Show WHERE Show_id IN (SELECT Show_id FROM Likes_Show WHERE Users_id = $1)",
+      [user_id]
+  );
+  return result.rows;
+};
+
+
 // Export functions for movies in ES Module syntax
-export { getAllMovies, createMovie, getLikedMovies};
+export { getAllMovies, createMovie, getLikedMovies, getLikedShow, createShow};
