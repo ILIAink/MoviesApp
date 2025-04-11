258208a6c8146316113de7383fb7931c9f3d08ae
julsnguyen <dpg4bz@virginia.edu>
2025-04-11 17:33:22 -0400

    reorganized watchmodeRoutes and movieRoutes

--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -1,19 +1,10 @@
-// import express from "express";
-// import { getAllMovies, createMovie } from "../controllers/movieController.js";
-
-// const router = express.Router();
-
-// router.get("/", getAllMovies);
-// router.post("/createMovie", createMovie);
-
-
-// export { router };
-
 import express from "express";
-import { fetchAndStoreTitle } from "../controllers/watchmodeController.js";
+import { getAllMovies, createMovie } from "../controllers/movieController.js";
 
 const router = express.Router();
 
-router.get("/searchTitle", fetchAndStoreTitle);
+router.get("/", getAllMovies);
+router.post("/createMovie", createMovie);
+
 
 export { router };
---
+++ backend/routes/watchmodeRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { fetchAndStoreTitle } from "../controllers/watchmodeController.js";
+
+const router = express.Router();
+
+router.get("/searchTitle", fetchAndStoreTitle); // expects ?query=title, e.g. /watchmode/searchTitle?query=inception
+
+export { router as watchmodeRouter };
